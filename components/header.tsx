d6949e22d9ca67b9c6bd9dbd6e5cf8dd206c52e2
Kelly Davis <kelly.davis@forestry.io>
2023-10-11 14:55:05 -0400

    setup

--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
                 You are not signed in
               </span>
               <a
-                href={`/api/auth/signin`}
+                href={`/api/foobar/auth/signin`}
                 className={styles.buttonPrimary}
                 onClick={(e) => {
                   e.preventDefault()
@@ -51,7 +51,7 @@ export default function Header() {
                 <strong>{session.user.email ?? session.user.name}</strong>
               </span>
               <a
-                href={`/api/auth/signout`}
+                href={`/api/foobar/auth/signout`}
                 className={styles.button}
                 onClick={(e) => {
                   e.preventDefault()
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session} basePath='/api/foobar/auth'>
       <Component {...pageProps} />
     </SessionProvider>
   )
